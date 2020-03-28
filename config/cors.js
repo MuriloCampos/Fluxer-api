ef05fc629fdfb1dd6dc0479994c9d724739b4001
Murilo Campos <murilo13campos@gmail.com>
2020-03-28 09:42:49 -0400

    testing cors

--- config/cors.js
+++ config/cors.js
@@ -16,7 +16,7 @@ module.exports = {
   | Function - Receives the current origin and should return one of the above values.
   |
   */
-  origin: ['https://pensive-kirch-1afd84.netlify.com'],
+  origin: ['https://pensive-kirch-1afd84.netlify.com', 'http://localhost:3000'],
 
   /*
   |--------------------------------------------------------------------------
